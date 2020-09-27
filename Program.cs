93e22890ef62b123a20448a2987801e17e67d76e
Marcos Villatoro <mvillatorol6@miumg.edu.gt>
2020-09-26 20:30:35 -0600

    git commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace parcial_2_Algoritmos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            Console.WriteLine("introduzca un numero");
+            num = Convert.ToInt32(System.Console.ReadLine());
+            Console.WriteLine($"la suma es: {suma(num)}");
+            Console.ReadKey();
+        }
+        public static long suma(int n)
+        {
+                if (n == 1){
+                    return 1;
+                 }
+                else
+                {
+                    return n + suma(n-1);
+                }
+            
+        }
+    }
+}
